1daf1b6c6d7386ea3299a73352e03b38a38fc4b0
Liam Cotter <122333311@umail.ucc.ie>
2023-05-23 20:35:44 +0100

    Restructured classes, added full data collection

--- orienteering.py
+++ orienteering.py
@@ -8,97 +8,92 @@ class UserResult:
         self.name = name
         self.search_term = search_term
         self.eventID = []
-        self.course = []
-        self.times = []
-        self.dates = []
+        self.eventResults = {}
+
+        self.compile_event_details()
+
+    def compile_event_details(self) -> None :
+        """Populates the attributes of the UserResult object"""
+
+        name = self.name.replace(" ", "+")
+        url = f"https://www.orienteering.ie/result2/?oaction=viewResults&league=&club=&map={self.search_term}&SMonth=&SYear=&FMonth=&FYear=&comptr={name}&start=0&XXX=Search"
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        r = get(url, headers=headers)
+        table = r.text.split("tbody>")[1]
+
+        rows = table.split("<tr>")[1:]
+        for entry in rows:
+            if "<td>" in entry and "DNF" not in entry:
+                    eventID = entry.split("moreResult&id=")[1].split('"')[0]
+                    result_info = entry.split("<td>")
+                    course, date, month = result_info[-2][:-5], result_info[1][:-5], result_info[2][:-5]
+                    time = result_info[-1].replace(" ", "").split("</td>")[0]
+                    time = int(time.split(":")[0])*60 + int(time.split(":")[1])
+                    date = datetime.strptime(f"{date} {month}", "%d %b")
+                    self.eventResults[eventID] = EventResult(eventID, course, time, date)
+                    self.eventResults[eventID].add_event_data()
 
 class EventResult:
-    def __init__(self, eventID : int, course : str, time : int, winning_time : int, distance : str, date : str):
+    def __init__(self, eventID : int, course : str, time : int, date : str):
         self.eventID = eventID
         self.course = course
         self.time = time
-        self.winning_time = winning_time
-        self.distance = distance
+        self.winning_time = None
+        self.distance = None
         self.date = date
         self.pace = None
         self.percent_behind = None
     
-    def set_calculations(self):
+    def set_calculations(self) -> None:
         """Sets the pace, winning_time attribute"""
         if self.distance is None:
             self.pace = None
         else:
             self.pace = self.time / self.distance # assume seconds per KM
-        self.percent_behind = 100* ((self.winning_time / self.time) -1)
+        self.percent_behind = 100* ((self.time / self.winning_time) -1)
+    
+    def add_event_data(self) -> None:
+        """Adds the winning time and distance to the EventResult object"""
+        df = get_df(self.eventID)
+        relevant_data = df[df["Course"] == self.course]
+        self.winning_time = get_best_time(relevant_data)
+        self.distance = get_distance(relevant_data)
+        self.set_calculations()
 
 # eventID = 22733
 def get_df(eventID : int) -> pd.DataFrame:
     """Fetches the results CSV"""
-    url = f"https://www.orienteering.ie/results/files/{str(eventID)[:3]}/{str(eventID)}.csv"
-    print(url)
+    url = f"https://www.orienteering.ie/results/files/{str(eventID)[:-2]}/{str(eventID)}.csv"
     headers = {'User-Agent': 'Mozilla/5.0'}
     r = get(url, headers=headers)
     fixed_csv = r.text.replace("(may be more) ...", "(may be more) ...;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")
     with open("test.csv", "w") as f:
         f.write(fixed_csv)
+    if "<title>" in fixed_csv:
+        # raise error message
+        pass
     df = pd.read_csv(StringIO(fixed_csv), sep=";").dropna(axis=1, how="all")
     return df
 
-def get_best_times(df : pd.DataFrame) -> int:
+def get_best_time(df : pd.DataFrame) -> int:
     """Gets the winning time on the course"""
-    times = df["Time"]
+    times = df["Time"].dropna()
+    #valid_times = times[times != "NaN"]
     times_in_seconds = times.transform(lambda t: int(t.split(":")[0])*60 + int(t.split(":")[1]))
     winning_time = min(times_in_seconds)
     return winning_time
 
 def get_distance(df : pd.DataFrame) -> float:
     """Gets the distance of the course"""
-    km = sum(df["km"]) or None
+    km = ""
+    if len(df["km"]) > 0:
+        if float(df["km"].iloc[0]) == 0:
+            km = None
+        else:
+            km = float(df["km"].iloc[0])
     return km
-    
-# to be a method of UserResult
-def compile_event_details(obj : UserResult) -> None :
-    """Populates the attributes of the UserResult object"""
-
-    name = obj.name.replace(" ", "+")
-    url = f"https://www.orienteering.ie/result2/?oaction=viewResults&league=&club=&map={obj.search_term}&SMonth=&SYear=&FMonth=&FYear=&comptr={name}&start=0&XXX=Search"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    r = get(url, headers=headers)
-    table = r.text.split("tbody>")[1]
-
-    id_list = table.split("moreResult&")[1:]
-    obj.eventID = [ id_str.split('"')[0][3:] for id_str in id_list ]
-    rows = table.split("<tr>")[1:]
-    for entry in rows:
-        print(entry)
-        if "<td>" in entry:
-            result_info = entry.split("<td>")
-            course, date, month = result_info[-2][:-5], result_info[1][:-5], result_info[2][:-5]
-            time = result_info[-1].replace(" ", "").split("</td>")[0]
-            if "DNF" in time:
-                time = None
-            else:
-                time = int(time.split(":")[0])*60 + int(time.split(":")[1])
-            obj.course.append(course)
-            obj.times.append(time)
-            date = datetime.strptime(f"{date} {month}", "%d %b")
-            obj.dates.append(date)
-            
-
-
 
 person = "Liam Cotter"
 search = "Fota%"
-obj = UserResult(person, search)
-compile_event_details(obj)
-print(obj.eventID)
-print(obj.course)
-index = 1
-# for loop
-df = get_df(obj.eventID[index])
-relevant_data = df[df["Course"] == obj.course[index]]
-winning_time = get_best_times(relevant_data)
-distance = get_distance(relevant_data)
-res = EventResult(obj.eventID[index], obj.course[index], obj.times[index], winning_time, distance, obj.dates[index])
-res.set_calculations()
-print(res)
+res = UserResult(person, search)
+
